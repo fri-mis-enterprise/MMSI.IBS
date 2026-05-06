b5655df1f629160f550dccd6a8c56fa87a0c3be0
Roi Borromeo <roi.for.school@gmail.com>
2026-05-06 11:42:11 +0800

    feat: add migration for jobid on billing model

---
+++ IBS.DataAccess/Migrations/20260506032105_AddJobOrderIdOnBilling.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobOrderIdOnBilling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "job_order_id",
+                table: "billings",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "job_order_id",
+                table: "billings");
+        }
+    }
+}
