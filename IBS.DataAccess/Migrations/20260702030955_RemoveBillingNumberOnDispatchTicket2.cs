a7f8e770fc63d68a39887680ce9ba9baa2330a87
Roi Borromeo <roi.for.work@gmail.com>
2026-07-02 11:11:30 +0800

    feat: remove the shadow job order id on migrations

---
+++ IBS.DataAccess/Migrations/20260702030955_RemoveBillingNumberOnDispatchTicket2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IBS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBillingNumberOnDispatchTicket2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_msap_dispatch_tickets_msap_job_orders_job_order_id1",
+                table: "msap_dispatch_tickets");
+
+            migrationBuilder.DropIndex(
+                name: "ix_msap_dispatch_tickets_job_order_id1",
+                table: "msap_dispatch_tickets");
+
+            migrationBuilder.DropColumn(
+                name: "job_order_id1",
+                table: "msap_dispatch_tickets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "job_order_id1",
+                table: "msap_dispatch_tickets",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_msap_dispatch_tickets_job_order_id1",
+                table: "msap_dispatch_tickets",
+                column: "job_order_id1");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_msap_dispatch_tickets_msap_job_orders_job_order_id1",
+                table: "msap_dispatch_tickets",
+                column: "job_order_id1",
+                principalTable: "msap_job_orders",
+                principalColumn: "job_order_id");
+        }
+    }
+}
